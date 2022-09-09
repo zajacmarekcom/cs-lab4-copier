a41226cbc16bce3fec6f6beda47ecc7ed3e4b22e
Marek Zajac <contact@zajacmarek.com>
2022-09-09 18:41:34 +0200

    Zadanie1

---
+++ Zadanie2/Copier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; }
+        public int ScanCounter { get; private set; }
+
+        public void Print(in IDocument document)
+        {
+            if (GetState() == IDevice.State.off)
+                return;
+            PrintCounter++;
+            Console.WriteLine($"{DateTime.Now.ToString("0:G")} Print: {document.GetFileName()}");
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            document = null;
+            if (GetState() == IDevice.State.off)
+                return;
+            ScanCounter++;
+            var extention = formatType.ToString().ToLower();
+            var filename = $"ImageScan{ScanCounter}.{extention}";
+            Console.WriteLine($"{DateTime.Now.ToString("0:G")} Scan: {filename}");
+
+            document = new ImageDocument(filename);
+        }
+
+        public void ScanAndPrint()
+        {
+            IDocument document;
+
+            Scan(out document, IDocument.FormatType.JPG);
+            Print(document);
+        }
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2, IDocument.FormatType.JPG);
+
+            xerox.ScanAndPrint();
+            Console.WriteLine(xerox.Counter);
+            Console.WriteLine(xerox.PrintCounter);
+            Console.WriteLine(xerox.ScanCounter);
+        }
+    }
+}
